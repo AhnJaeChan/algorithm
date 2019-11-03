57b069af57fbce2704919cd9c7820eb2c22ee939
Ahn Jaechan <jaechan5256@gmail.com>
2019-11-04 03:06:35 +0900

    implementation accepted

--- problems/leetcode/reverse-integer/main.cpp
+++ problems/leetcode/reverse-integer/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  int reverse(int x) {
+    int result = 0;
+
+    while (x != 0) {
+      int digit = x % 10;
+
+      if (result > INT32_MAX / 10 || (result == INT32_MAX / 10 && digit > 7)) {
+        return 0;
+      }
+
+      if (result < INT32_MIN / 10 || (result == INT32_MIN / 10 && digit < -8)) {
+        return 0;
+      }
+
+      result = result * 10 + digit;
+      x /= 10;
+    }
+
+    return result;
+  }
+};
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  Solution solution;
+
+  std::cout << solution.reverse(-123) << std::endl;
+
   return 0;
 }
